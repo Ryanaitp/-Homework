e3f3376d18904e2d22e63eeb3558d85dc8d7cc74
Alyona <ryanaitp@gmail.com>
2019-11-22 10:18:26 +0300

    second commit

---
+++ JSExample.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let money = +prompt ("Ваш бюджет на месяц?", ""),
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+
+    
--- script (1).js
+++ script (1).js
@@ -14,7 +14,7 @@ let appData = {
 };
 
 
-/*for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 2; i++) {
     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
         b = prompt ("Во сколько обойдется?", "");
 
@@ -28,44 +28,52 @@ let appData = {
         i--;
     }
 
-};*/
+};
 
- let i = 0;
-while (i < 2) {
-    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt ("Во сколько обойдется?", "");
 
-    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
 
-        console.log ("done");
+// Используем цикл WHILE
 
-         appData.expenses[a] = b;
-     } else {
-         console.log ("bad result");
-         i--;
-     }
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
 
-   i++;
- }
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
 
- /*let i = 0;
- do {
-     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-         b = prompt ("Во сколько обойдется?", "");
+//         console.log ("done");
 
-     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
 
-        console.log ("done");
+//     i++;
+// }
 
-         appData.expenses[a] = b;
-    } else {
-          console.log ("bad result");
-          i--;
-     }
 
-     i++;
- }
- while(i < 2);*/
+
+// Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
 
 
 appData.moneyPerDay = appData.budget / 30;
